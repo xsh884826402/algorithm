002703058cf6829c82908174c1aa7fc70ff962fe
xushenghua <xushenghua@chinapost.com.cn>
2023-03-01 14:58:35 +0800

    修改insertionSort.py

--- sort/insertionSort.py
+++ sort/insertionSort.py
@@ -1,17 +1,16 @@
-nums = []
-
-
-def sort(nums):
+def insertion_sort(nums):
     lenn = len(nums)
     if lenn==0 or lenn == 1:
         return nums
 
-    for l in range(1, lenn):
-        for index in range(l-1, -1, 1):
-            if nums[index+1]< nums[index]:
-                nums[index+1], nums[index] = nums[index], nums[index+1]
+    for l in range(2, lenn+1):
+        for index in range(l-1, 0, -1):
+            if nums[index] < nums[index-1]:
+                nums[index], nums[index-1] = nums[index-1], nums[index]
     return nums
 
 
-print(sort([7, 6, 5, 4, 3, 2,1]))
+nums1 = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+nums2 = [7, 8, 9, 4, 5, 6, 1, 2, 3]
+print(insertion_sort(nums2))
 # 最坏复杂度 o(n^2)
