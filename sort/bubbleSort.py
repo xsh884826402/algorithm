2ff03af0310ada6c0360e4f05cbbbd355d387810
xushenghua <xushenghua@chinapost.com.cn>
2023-03-01 14:57:58 +0800

    添加冒泡排序

---
+++ sort/bubbleSort.py
@@ -0,0 +1,14 @@
+def bubble_sort(nums):
+    lenn = len(nums)
+    if lenn==0 or lenn==1:
+        return nums
+    for l in range(lenn, 0, -1):
+        for index in range(0, l-1, 1):
+            if nums[index] > nums[index+1]:
+                nums[index], nums[index+1] = nums[index+1], nums[index]
+    return nums
+
+
+nums1 = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+nums2 = [7, 8, 9, 4, 5, 6, 1, 2, 3]
+print(bubble_sort(nums2))
